b925964ad81c90baa836405d3de00ae32b15f0b2
numata <nununu.mono@gmail.com>
2018-05-02 14:07:42 +0900

    future: add CRUD attribution

--- backlogapi/resourse/base.py
+++ backlogapi/resourse/base.py
@@ -7,6 +7,8 @@ class BacklogBase:
     """
     Base class is identified by id and class name
     """
+    endpoint = None
+
     def __init__(self, client):
         self.client = client
         self.id = None
@@ -33,3 +35,50 @@ class BacklogBase:
         for key, res in self._attr:
             setattr(self, key, response.get(res, None))
         return self
+
+    def all(self):
+        """
+        Get all object
+        """
+        res = self.client.fetch_json(self.endpoint, method='GET')
+        return [self.__class__(self.client).from_json(x) for x in res]
+
+    def get(self, id_):
+        """
+        Get one object
+        """
+        res = self.client.fetch_json('/'.join([self.endpoint, id_]), method='GET')
+        return self.__class__(self.client).from_json(res)
+
+    def filter(self, **params):
+        """
+        Get filtering object
+        :return:
+        """
+        res = self.client.fetch_json(self.endpoint, method='GET', query_params=params)
+        return [self.__class__(self.client).from_json(x) for x in res]
+
+    def create(self, **params):
+        """
+        Create new object
+        """
+        res = self.client.fetch_json(self.endpoint, method='POST', post_params=params)
+        return self.__class__(self.client).from_json(res)
+
+    def update(self, id_=None, **params):
+        """
+        Update the object
+        """
+        if self.id is not None:
+            res = self.client.fetch_json(f'{self.endpoint}/{self.id}', method='POST', post_params=params)
+        else:
+            res = self.client.fetch_json(f'{self.endpoint}/{id_}', method='POST', post_params=params)
+        return self.__class__(self.client).from_json(res)
+
+    def delete(self, id_=None):
+        if self.id is not None:
+            res = self.client.fetch_json(f'{self.endpoint}/{self.id}', method='DELETE')
+        else:
+            res = self.client.fetch_json(f'{self.endpoint}/{id_}', method='DELETE')
+        return self.__class__(self.client).from_json(res)
+
--- backlogapi/utilities.py
+++ backlogapi/utilities.py
@@ -12,8 +12,11 @@ def protect(roles):
     def _protect(func):
         @wraps(func)
         def __protect(self, *args, **kwargs):
-            print(self.client.role, roles)
-            if self.client.role not in roles:
+            if self.__class__.__name__ == 'BacklogClient':
+                role = self.role
+            else:
+                role = self.client.role
+            if role not in roles:
                 raise exceptions.UnauthorizedOperationError
             return func(self, *args, **kwargs)
         return __protect
